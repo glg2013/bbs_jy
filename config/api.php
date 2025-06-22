b2271e11339ec71438d1b863ffa93afdb90b3e7c
glg2013 <57412960@qq.com>
2025-06-22 04:47:24 +0000

    频率限制

---
+++ config/api.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    /**
+     * 接口频率限制
+     */
+    'rate_limit' => [
+        // 访问频率限制，次数/分钟
+        'access'    =>  env('RATE_LIMIT', '60,1'),
+        // 登录相关，次数/分钟
+        'sign'  =>  env('SIGN_RATE_LIMIT', '10,1'),
+    ]
+];
