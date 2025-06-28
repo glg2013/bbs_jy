81bd5be238862267f710514c53167940236cac42
glg2013 <57412960@qq.com>
2025-06-28 05:56:15 +0000

    话题详情

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Queries\TopicQuery;
 use App\Http\Requests\Api\TopicRequest;
 use App\Http\Resources\TopicResource;
 use App\Models\Topic;
@@ -14,18 +15,24 @@ use Spatie\QueryBuilder\QueryBuilder;
 class TopicsController extends Controller
 {
 
-    public function index(Request $request, Topic $topic)
+    public function index(Request $request, TopicQuery $query)
     {
-        $topics = QueryBuilder::for(Topic::class)
-            ->allowedIncludes('user', 'category')
-            ->allowedFilters([
-                'title',
-                AllowedFilter::exact('category_id'),
-                AllowedFilter::scope('withOrder')->default('recentReplied'),
-            ])
-            ->paginate();
+        $topics = $query->paginate();
 
         return TopicResource::collection($topics);
+    }
+
+    public function userIndex(Request $request, User $user, TopicQuery $query)
+    {
+        $topics = $query->where('user_id', $user->id)->paginate();
+
+        return TopicResource::collection($topics);
+    }
+
+    public function show($topicId, TopicQuery $query)
+    {
+        $topic = $query->findOrFail($topicId);
+        return new TopicResource($topic);
     }
 
     public function store(TopicRequest $request, Topic $topic)
@@ -55,21 +62,5 @@ class TopicsController extends Controller
 
         $topic->delete();
         return response(null, 204);
-    }
-
-    public function userIndex(Request $request, User $user)
-    {
-        $query = $user->topics()->getQuery();
-
-        $topics = QueryBuilder::for($query)
-            ->allowedIncludes('user', 'category')
-            ->allowedFilters([
-                'title',
-                AllowedFilter::exact('category_id'),
-                AllowedFilter::scope('withOrder')->default('recentReplied'),
-            ])
-            ->paginate();
-
-        return TopicResource::collection($topics);
     }
 }
